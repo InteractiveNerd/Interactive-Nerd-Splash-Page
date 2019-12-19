c63af0c213fb82170ec48fef040932e7060ff4af
Paul Saada <paulsaada6@gmail.com>
2019-12-19 00:26:49 -0500

    added logo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="App">
+      <Logo />
     </div>
   );
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../stylesheets/Logo.css'
+
+const Logo = () => {
+  return (
+    <div className="logo">in.</div>
+  );
+}
+
+export default Logo;
