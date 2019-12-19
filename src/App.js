358eae8439e46bfbc74f9596eb93cfc5bbb55f01
Paul Saada <paulsaada6@gmail.com>
2019-12-19 00:37:32 -0500

    added timer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Logo from './components/Logo';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <Logo />
+      <Timer />
     </div>
   );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/Timer.css';
+
+const Timer = () => {
+  return (
+    <div>
+      06
+    </div>
+  );
+}
+
+export default Timer;
