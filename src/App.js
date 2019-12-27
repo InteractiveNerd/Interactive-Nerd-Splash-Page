32ce6b59e6759742943573ed7b8be7d2e1a6b491
Paul Saada <paulsaada6@gmail.com>
2019-12-27 17:34:32 -0500

    added footer styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import Logo from './components/Logo';
-import Divider from './components/Divider';
-import FooterTitle from './components/FooterTitle';
-import FooterSubtitle from './components/FooterSubtitle';
 import AppContainer from './containers/AppContainer';
+import Footer from './containers/FooterContainer';
 
 function App() {
   return (
     <div className="App">
       <Logo />
       <AppContainer />
-      <Divider />
-      <FooterTitle />
-      <FooterSubtitle />
+      <Footer />
     </div>
   );
 }
