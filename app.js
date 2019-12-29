a091fa65939173c1ecf5d23b42674eb34b3f93b0
Paul Saada <paulsaada6@gmail.com>
2019-12-29 05:40:41 -0500

    connected frontend to express server

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const path = require('path');
 const app = express();
 
 // Static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const port = process.env.port || 5000;
 app.listen(port, console.log(`Server started on ${port}`));
