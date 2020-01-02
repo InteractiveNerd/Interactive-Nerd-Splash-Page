35f190dce669e1f5221ec8ce4c72ac38921c0df5
Paul Saada <paulsaada6@gmail.com>
2020-01-02 12:35:58 -0500

    resized logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import '../stylesheets/Logo.css'
 const Logo = () => {
   return (
     <div className="logo bolded--text">
-      <a href="index.html">in.</a>
+      <a href="index.html">interactive<span className="logo--emphasize">nerd</span></a>
     </div>
   );
 }
